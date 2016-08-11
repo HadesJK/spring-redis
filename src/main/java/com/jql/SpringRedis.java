ef4599bf01bfcf3963d7b38cb80b9b377849a107
金奇樑 <hzjinqiliang@corp.netease.com>
2016-08-11 16:44:25 +0800

    spring redis

---
+++ src/main/java/com/jql/SpringRedis.java
@@ -0,0 +1,31 @@
+package com.jql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by 金奇樑(hzjinqiliang)
+ * 16/8/11 14:43
+ */
+@Service
+public class SpringRedis {
+
+    @Autowired
+    private RedisTemplate<String, String> template;
+
+    @Resource(name = "redisTemplate")
+    private ListOperations<String, Object> listOps;
+
+    @Resource(name = "redisTemplate")
+    private ValueOperations valueOps;
+
+    @Resource(name = "redisTemplate")
+    private HashOperations<String, String, String> hashOps;
+
+}
